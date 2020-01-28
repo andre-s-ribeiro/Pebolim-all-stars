bc7db694c8285b0ad6ee315579aeb5981bf47c41
andre <andresantosarkanjo8@gmail.com>
2020-01-28 18:08:20 -0300

    feat: logica configurações tela placar quase feita

--- Pebolim-all-stars/src/app/placar-modal-content/placar-modal-content.component.ts
+++ Pebolim-all-stars/src/app/placar-modal-content/placar-modal-content.component.ts
@@ -8,15 +8,20 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./placar-modal-content.component.scss'],
 })
 export class PlacarModalContentComponent implements OnInit {
-
+  firstOption: string = 'limite de tempo';
+  secondOption: string = 'limite de pontos';
+  tempoLimite: boolean = false;
+  pontosLimite: boolean = false;
   constructor(
     private router: Router,
     private modalCtrl: ModalController
-    ) { }
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
 
-  sair(){
+  }
+
+  sair() {
     this.router.navigate(['tela-inicio']);
     this.dismiss();
   }
@@ -25,5 +30,18 @@ export class PlacarModalContentComponent implements OnInit {
     this.modalCtrl.dismiss({
       'dismissed': true
     });
+  }
+
+  onChange($event){
+    if($event.detail.value[1]){
+      this.tempoLimite = true;
+      this.pontosLimite = true;
+    }else if($event.detail.value[0] == this.firstOption){
+      this.tempoLimite = true;
+      this.pontosLimite = false;
+    }else{
+      this.tempoLimite = false;
+      this.pontosLimite = true;
+    }
   }
 }

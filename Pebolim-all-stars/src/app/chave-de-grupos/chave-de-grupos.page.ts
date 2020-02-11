7adad180e2c69ce4efb6571a598c7ca9bed89442
andre <andresantosarkanjo8@gmail.com>
2020-02-11 11:05:33 -0300

    chore: limpando codigo

--- Pebolim-all-stars/src/app/chave-de-grupos/chave-de-grupos.page.ts
+++ Pebolim-all-stars/src/app/chave-de-grupos/chave-de-grupos.page.ts
@@ -1,16 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ChaveGruposService } from '../shared/services/chave-grupos.service';
 import { Grupos } from '../shared/model/grupos.model';
-import { Times } from '../shared/model/times.model';
 
 
 @Component({
   selector: 'app-chave-de-grupos',
   templateUrl: './chave-de-grupos.page.html',
   styleUrls: ['./chave-de-grupos.page.scss'],
-  providers: [
-    ChaveGruposService
-  ]
 })
 export class ChaveDeGruposPage implements OnInit {
   grupos: Grupos[] = [];
@@ -25,7 +21,6 @@ export class ChaveDeGruposPage implements OnInit {
   getGrupos() {
    this.chaveGrupoService.getGrupos().then((grupos: Grupos[])=>{
       this.grupos = grupos
-
    })
   }
 }
